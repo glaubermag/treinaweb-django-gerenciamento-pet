03a997f0640e34e49a95b332154ca5001e33be9e
Glauber Ariel Magalhães <glaubermagalhaes@outlook.com>
2021-01-13 20:37:32 -0300

    install necessary dependencies

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\glaub\PycharmProjects\treinaweb-django-gerenciamento-pet\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
